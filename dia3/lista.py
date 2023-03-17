0b4249af785df6a1ee840b923f62e3ba410cd18e
miguel m <miguelmonsalvo19@gmail.com>
2023-03-17 03:26:53 -0500

    dia3.1

---
+++ dia3/lista.py
@@ -0,0 +1,3 @@
+mi_lista = ['a','b','c']
+otra_lista = ['hola',55,6.1]
+print(mi_lista)
---
+++ diccionarios.py
@@ -0,0 +1,9 @@
+diccionario = {'c1':'valor','c2':'valor2' }
+cliente = {'nombre':'juan','apellido':'fuentes','peso':88,'talla':1.77}
+consulta = cliente['apellido']
+# print(consulta)
+
+dic2 = {'c1':55,'c2':[10,20,30],'c3':{'s1':100,'s2':200}}
+
+dic = {'c1':['a','b','c'],'c2':['d','e','f']}
+print(dic['c2'][1].upper())
