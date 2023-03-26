03284184a10bb3db6de58aaae967dc26a07a8a7a
memonsalvo <miguelmonsalvo19@gmail.com>
2023-03-26 04:35:01 -0500

    4

---
+++ dia4/operadores_de_comparacion.py
@@ -0,0 +1,5 @@
+mi_variable = 'hola mundo'
+mi_bool = 10 == 25 #igual que
+mi_bool2 = 5+5==18-8
+mi_bool3 = 'blanco' == 'negro'
+mi_bool4 = 100!=100
---
+++ dia4/operadores_logicos.py
@@ -0,0 +1,2 @@
+mi_bool = (55 == 55) and ('perro' == 'perro')
+mi_bool2 = 1 == 10 or 3 == 30
