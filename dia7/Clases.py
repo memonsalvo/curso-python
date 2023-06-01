24d18907c0bd599efb8f03510b7ee18dce55ed1c
memonsalvo <miguelmonsalvo19@gmail.com>
2023-06-01 03:18:42 -0500

    7.0

---
+++ dia7/Clases.py
@@ -0,0 +1,7 @@
+class Pajaro:
+    pass
+
+mi_pajaro = Pajaro()
+otro_pajaro = Pajaro() #las variables dejan de serlo y se llaman instancias de una clase
+# todas son parte de la clase pajaro pero cada una es diferente de la otra
+print(mi_pajaro)
