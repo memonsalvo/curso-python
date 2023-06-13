ab586994806ca10de3368878ccf653394d9e0f88
memonsalvo <miguelmonsalvo19@gmail.com>
2023-06-13 04:07:44 -0500

    7.6

---
+++ dia7/Polimorfismo.py
@@ -0,0 +1,27 @@
+class Vaca:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        
+    def hablar(self):
+        print(self.nombre + ' dice muuu')
+        
+class Oveja:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        
+    def hablar(self):
+        print(self.nombre + ' dice beee')
+        
+vaca1 = Vaca('Aurora')
+
+oveja1 = Oveja('nube')
+
+# animales = [vaca1, oveja1]
+
+# for animal in animales:
+#     animal.hablar()
+
+def animal_habla(animal):
+    animal.hablar()
+    
+animal_habla(oveja1)
